f6c0fae983b1b8d68c34b0ba255b5660d19a2d5e
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-03-14 17:43:00 +0530

    CHakra ui configuration

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 function App() {
   const dispatch = useDispatch();
   const state = useSelector(state=>state);
+
+
   console.log(state)
   return (
     <div className="App">
       <Navbar/>
+      hee
     </div>
   );
 }
--- src/Router/Navbar.jsx
+++ src/Router/Navbar.jsx
@@ -1,8 +1,29 @@
-import { NavLink } from 'react-router-dom';
-import { Flex, Spacer, Link } from '@chakra-ui/react';
+import {Box,Image,useColorMode,Link,Button} from '@chakra-ui/react';
+import {BsSunFill,BsMoonFill} from 'react-icons/bs';
+import logo from '../images/logo.png';
 
 function Navbar() {
-  return <></>
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  console.log(colorMode)
+
+  return <>
+  <Box w='100%' display='flex' px={{sm:'10px',lg:'30px'}} py={{sm:'10px'}} justifyContent='space-between' alignItems='center'>
+    <Box display="flex" alignItems="center" gap='10px'>
+      <Image width={{sm:"50px"}} objectFit={'cover'} src={logo}/>
+      <p style={{
+        fontWeight:"bold"}}
+        className="logotxt"
+      >Z O S T E L</p>
+    </Box>
+    <Box>
+      
+      <Button onClick={toggleColorMode}>
+         { colorMode==="light"?<BsMoonFill />:<BsSunFill fontSize={'1.4rem'}/>}
+      </Button>
+    </Box>
+  </Box>
+  </>
 }
 
 export default Navbar;
