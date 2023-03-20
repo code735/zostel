39b3622e71b0b8d600aceeaf0e72dfb0bbc2e2fe
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-03-20 16:49:42 +0530

    logo and title change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 import Navbar from './Router/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import AllRoutes from './Router/AllRoutes';
 import Footer from './Router/Footer'
+import { Helmet } from 'react-helmet';
+
 
 function App() {
   const dispatch = useDispatch();
@@ -13,6 +15,9 @@ function App() {
   console.log(state)
   return (
     <div className="App">
+      <Helmet>
+        <link rel="icon" type="image/png" href={logo} sizes="16x16" />
+      </Helmet>
       <Navbar />
       <AllRoutes />
       <Footer />
