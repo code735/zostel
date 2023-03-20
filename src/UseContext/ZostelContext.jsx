4790741c2c4e0d3bafbed85c6924947f2c494d81
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-03-20 14:01:32 +0530

    Payment page Dynamic

--- src/UseContext/ZostelContext.jsx
+++ src/UseContext/ZostelContext.jsx
@@ -5,9 +5,10 @@ export const ZostelContext= createContext()
 
 const ZostelContextProvider = ({children}) => {
   const [isLogin,setIsLogin]=useState(false)
+  const [maincart,setMaincart]=useState([])
 
   return (
-    <ZostelContext.Provider value={{isLogin,setIsLogin}} >
+    <ZostelContext.Provider value={{isLogin,setIsLogin,maincart,setMaincart}} >
         {children}
     </ZostelContext.Provider>
   )
